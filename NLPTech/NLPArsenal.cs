924e277508942942aba0c1c0fb667b55af5f9b22
ExplorerFreda <hyshi@pku.edu.cn>
2016-11-24 10:57:09 +0800

    version1
    version 1: build the sentence segmenter and the tokenizer.

---
+++ NLPTech/NLPArsenal.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+using opennlpinterface;
+
+namespace NLPTech
+{
+    public class NLPArsenal
+    {
+        NLPToolBox player = null;
+
+        public NLPArsenal(string[] model_paths)
+        {
+            player = new NLPToolBox(model_paths[0], model_paths[1], model_paths[2], model_paths[3], model_paths[4], model_paths[5], model_paths[6], model_paths[7], model_paths[8], model_paths[9], model_paths[10], model_paths[11], model_paths[12].Substring(0, model_paths[12].Length - 4));
+        }
+
+        public opennlp.tools.util.Span[] SentenceSegmentation(string text)
+        {
+            return player.sentenceDetector.sentPosDetect(text);
+        }
+
+        public opennlp.tools.util.Span[] Tokenization(string s)
+        {
+            return player.sentenceTokenizer.tokenizePos(s);
+        }
+
+        public string[] PartOfSpeechTagging(string[] tks)
+        {
+            return player.tagger.tag(tks);
+        }
+
+        public opennlp.tools.util.Span[] PersonDetection(string[] tks)
+        {
+            return player.persontagger.find(tks);
+        }
+
+        public opennlp.tools.util.Span[] OrganizationDetection(string[] tks)
+        {
+            return player.orgtagger.find(tks);
+        }
+
+        public opennlp.tools.util.Span[] LocationDetection(string[] tks)
+        {
+            return player.loctagger.find(tks);
+        }
+
+        public string[] Chunking(string[] tks, string[] postags)
+        {
+            return player.chunker.chunk(tks, postags);
+        }
+
+        /*
+        CoNLL format input:
+         * tab seperated 6 columns
+            * index (not zero-based)
+            * token
+            * _
+            * POS tag (Penn Treebank POS tagset)
+            * POS tag (must be exactly same with the 4th column)
+            * _
+        
+        CoNLL format output:
+         * tab seperated columns
+         * get non zero-based index of parent node and,
+         * the dependency
+         * as follow:
+        int[] parents = new int[conlls.Length];
+        string[] deptypes = new string[conlls.Length];
+        for (int i = 0; i < conlls.Length; i++)
+        {
+            string[] eles = conlls[i].Split(seperator_tab, StringSplitOptions.RemoveEmptyEntries);
+            parents[i] = int.Parse(eles[6]);
+            deptypes[i] = eles[7];
+        }
+        */
+        public string[] DependencyParsing(string[] tks, string[] postags)
+        {
+            string[] conlls = new string[tks.Length];
+
+            if (conlls.Length == 0) return conlls;
+
+            for (int i = 0; i < conlls.Length; i++)
+            {
+                conlls[i] = (i + 1).ToString() + "\t" + (tks[i] == "" ? "_" : tks[i]) + "\t_\t" + postags[i] + "\t" + postags[i] + "\t_";
+            }
+
+            return player.maltparser.parseTokens(conlls);
+        }
+    }
+}
---
+++ Pacer/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Threading;
+
+using opennlpinterface;
+
+namespace Pacer
+{
+    public class NLPTool
+    {
+        NLPToolBox nlp_models = null;
+        public NLPTool(string SSModel, string TKModel, string POSModel,
+            string CKModel, string PersonModel, string OrgModel, string LocModel,
+            string DateModel, string MoneyModel, string PercentageModel, string TimeModel,
+            string ParseModel, string MaltParseModel)
+        {
+            nlp_models = new NLPToolBox(SSModel, TKModel, POSModel, CKModel, PersonModel,
+                OrgModel, LocModel, DateModel, MoneyModel, PercentageModel, TimeModel,
+                ParseModel, MaltParseModel);
+        }
+        public List<string> Tokenize(string sentence)
+        {
+            List<string> ret = new List<string>();
+            opennlp.tools.util.Span[] tkpos =
+                nlp_models.sentenceTokenizer.tokenizePos(sentence);
+            for (int i = 0; i < tkpos.Length; i++)
+            {
+                ret.Add(sentence.Substring(tkpos[i].getStart(), tkpos[i].length()));
+            }
+            return ret;
+        }
+        public List<string> SentenceSegment(string raw_corpus)
+        {
+            List<string> ret = new List<string>();
+            opennlp.tools.util.Span[] ss =
+                nlp_models.sentenceDetector.sentPosDetect(raw_corpus);
+            for (int i = 0; i < ss.Length; i++)
+            {
+                ret.Add(raw_corpus.Substring(ss[i].getStart(), ss[i].length()));
+            }
+            return ret;
+        }
+    }
+    class Program
+    {
+        static string output_folder = @"..\..\..\Data\ProcessedCorpus\Tokenization\";
+        static string corpus_filename = @"..\..\..\Corpus\toy_corpus.txt";
+        // static string corpus_filename = @"..\..\..\Corpus\WestburyLab.wikicorp.201004.txt";
+        static void Main(string[] args)
+        {
+            // Build the tokenizer
+            string toolbox_folder = @"..\..\..\NLPTech\Toolbox\";
+            string SSModel = toolbox_folder + "en-sent.bin";
+            string TKModel = toolbox_folder + "en-token.bin";
+            string POSModel = toolbox_folder + "en-pos-maxent.bin";
+            string CKModel = toolbox_folder + "en-chunker.bin";
+            string PersonModel = toolbox_folder + "en-ner-person.bin";
+            string OrgModel = toolbox_folder + "en-ner-organization.bin";
+            string LocModel = toolbox_folder + "en-ner-location.bin";
+            string DateModel = toolbox_folder + "en-ner-date.bin";
+            string MoneyModel = toolbox_folder + "en-ner-money.bin";
+            string PercentageModel = toolbox_folder + "en-ner-percentage.bin";
+            string TimeModel = toolbox_folder + "en-ner-time.bin";
+            string ParseModel = toolbox_folder + "en-parser-chunking.bin";
+            string MaltParseModel = toolbox_folder + "engmalt.linear-1.7.mco";
+            NLPTool nlptool = new NLPTool(SSModel, TKModel, POSModel, CKModel,
+                PersonModel, OrgModel, LocModel, DateModel, MoneyModel, PercentageModel,
+                TimeModel, ParseModel, MaltParseModel);
+            // Load the corpus
+            string output_filename = output_folder + Path.GetFileName(corpus_filename);
+            StreamReader fin = new StreamReader(corpus_filename);
+            StreamWriter fout = new StreamWriter(output_filename);
+            for (string line = fin.ReadLine(); line != null; line = fin.ReadLine())
+            {
+                line = line.Replace('"', ' ');
+                List<string> sentences = nlptool.SentenceSegment(line);
+                foreach (string sentence in sentences)
+                {
+                    List<string> words = nlptool.Tokenize(sentence);
+                    foreach (string word in words)
+                    {
+                        try
+                        {
+                            byte[] bArray = Encoding.UTF8.GetBytes(word);
+                            foreach (byte bElement in bArray)
+                            {
+                                fout.Write((char)bElement);
+                            }
+                            fout.Write(" ");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("{0} Exception caught.", e);
+                        }
+                    }
+                }
+                fout.WriteLine();
+            }
+            fin.Close();
+            fout.Close();
+        }
+    }
+}
